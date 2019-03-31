867e862e605e4ffbb53468c75b3ab5e44639ab7b
nzerr <nicolas.zerr@gmail.com>
2019-03-31 21:15:43 +0200

    Updates

---
+++ precache-manifest.842db01f1b8ad9d420b6201291a7c17c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "59e6a937045a7c8d8dcd938c0ea44dfd",
+    "url": "/fls-memory-module/static/media/quantum01.59e6a937.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fls-memory-module/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0c7595a4b80ff8b984ab",
+    "url": "/fls-memory-module/static/js/runtime~main.93ab4745.js"
+  },
+  {
+    "revision": "fb931866a858bdd2c9bd",
+    "url": "/fls-memory-module/static/js/main.b944bc9a.chunk.js"
+  },
+  {
+    "revision": "e9cacbf2bc110e26e9e8",
+    "url": "/fls-memory-module/static/js/2.dfb0cf60.chunk.js"
+  },
+  {
+    "revision": "fb931866a858bdd2c9bd",
+    "url": "/fls-memory-module/static/css/main.b6a2ab54.chunk.css"
+  },
+  {
+    "revision": "5fd4ee83f0272cb8ce93440042fdff48",
+    "url": "/fls-memory-module/index.html"
+  }
+];
